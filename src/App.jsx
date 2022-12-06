169bb22d331be6fe79d5d1a8e7cce65ad0d68aa4
Istiak Hassan Emon <emon5122@gmail.com>
2022-12-07 02:58:46 +0600

    Perfect
    Signed-off-by: Istiak Hassan Emon <emon5122@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,34 @@ import React, { useState, useEffect } from "react";
 import Map from "./Map";
 
 const App = () => {
-    // const Dhaka = { latitude: 23.8103, longitude: 90.4125 };
     const [location, setLocation] = useState();
 
     useEffect(() => {
-      // check if geolocation is supported by the browser
-      if ('geolocation' in navigator) {
-        // get the current position
-        navigator.geolocation.getCurrentPosition((position) => {
-          // update the state with the current position
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          });
-        });
-    
-        // watch for changes to the position and update the state
-        const watchId = navigator.geolocation.watchPosition((position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          });
-        });
-    
-        // return a cleanup function to stop watching for changes
-        // when the component unmounts
-        return () => navigator.geolocation.clearWatch(watchId);
-      }
+        // check if geolocation is supported by the browser
+        if ("geolocation" in navigator) {
+            // get the current position
+            navigator.geolocation.getCurrentPosition((position) => {
+                // update the state with the current position
+                setLocation({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                });
+            });
+
+            // watch for changes to the position and update the state
+            const watchId = navigator.geolocation.watchPosition((position) => {
+                setLocation({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                });
+            });
+
+            // return a cleanup function to stop watching for changes
+            // when the component unmounts
+            return () => navigator.geolocation.clearWatch(watchId);
+        } 
     }, []);
+    
     return (
         <div>
             <Map latlng={location} zoom={13} />
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 import { Marker } from "react-leaflet";
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
-import "./Map.css"
+import "./Map.css";
 
 export default function Map({ latlng, zoom }) {
+    const centered = latlng
+        ? latlng
+        : {
+            lat: 23.8103,
+            lng: 90.4125,
+        };
+        console.log(centered)
     return (
-      <MapContainer center={[latlng.latitude, latlng.longitude]} zoom={zoom}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[latlng.latitude, latlng.longitude]} />
-      </MapContainer>
+        <MapContainer center={centered} zoom={zoom}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={centered} />
+        </MapContainer>
     );
 }
