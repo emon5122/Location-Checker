4b9b5a0faa14fec027e047fbefbb4721ac9ff25d
emon5122 <emon5122@gmail.com>
2022-12-06 19:17:05 +0600

    Test
    Signed-off-by: emon5122 <emon5122@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
-import React, { useEffect, useState } from "react";
-import MyMap from "./MyMap";
-import "../tailwind.css";
+import React, { useEffect, useMemo, useState } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
-function App() {
-  const [location, setLocation] = useState(null);
+const MyMap = ({ latlng, zoom }) => {
+  // Use the useState hook to track the latlng value, with a default value of null
 
+  const [mapLatLng, setMapLatLng] = useState({ lat: 23.8103, lng: 90.4125 });
+
+  // Use the useEffect hook to update the mapLatLng value when the latlng prop changes
   useEffect(() => {
-    navigator.geolocation.watchPosition(function (position) {
-      setLocation(position.coords);
-    });
+    if (latlng && latlng.lat && latlng.lng) {
+      setMapLatLng(latlng);
+    }
+  }, [latlng]);
+  
+  // Use the useMemo hook to memoize complex calculations
+  const center = useMemo(() => [mapLatLng.lat, mapLatLng.lng], [mapLatLng]);
+
+  return (
+    <MapContainer center={center} zoom={zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={center} />
+    </MapContainer>
+  );
+};
+
+
+const App = () => {
+  // Use the useState hook to track the user's current location
+  const [location, setLocation] = useState(null);
+  // MyMap.defaultProps = {
+  //   latlng: { lat: 23.8103, lng: 90.4125 }
+  // };
+  
+
+  // Use the useEffect hook to get the user's location when the component is mounted
+  useEffect(() => {
+    async function watchPosition() {
+      navigator.geolocation.watchPosition(function (position) {
+        setLocation(position.coords);
+      });
+    }
+    watchPosition();
   }, []);
+  
 
   return (
     <div className="map-container">
@@ -20,6 +56,6 @@ function App() {
       )}
     </div>
   );
-}
+};
 
 export default App;
